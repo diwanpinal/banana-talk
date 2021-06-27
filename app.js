27f24d51cddfbb5fcf722012dc1b1015a26bcdae
diwanpinal <diwanpinal@gmail.com>
2021-06-28 00:15:38 +0530

    docs- added button

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ console.log("hey");
 var Username = prompt("give me username")
 var WelcomeMessage = "this script works.." + Username;
 alert(WelcomeMessage);
+var btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate);
+btnTranslate.addEventListenerpredifined{"click".function clickEventHandler() {
+    console.log("clicked")
+} }
